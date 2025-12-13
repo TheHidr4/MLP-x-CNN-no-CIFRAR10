84c5446eb1c97cb440675d4b2d27cbcebec49b14
TheHidr4 <paulovicente.7ano@gmail.com>
2025-12-13 20:25:24 -0300

    Adição de funcionalidades

---
+++ src/data.py
@@ -0,0 +1,35 @@
+# src/data.py
+import numpy as np
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    classes = [0, 1, 2, 3, 4]
+
+    mask_train = np.isin(y_train, classes).flatten()
+    mask_test = np.isin(y_test, classes).flatten()
+
+    x_train = x_train[mask_train]
+    y_train = y_train[mask_train]
+    x_test = x_test[mask_test]
+    y_test = y_test[mask_test]
+
+    val_size = int(0.1 * len(x_train))
+
+    x_val = x_train[-val_size:]
+    y_val = y_train[-val_size:]
+
+    x_train = x_train[:-val_size]
+    y_train = y_train[:-val_size]
+
+    y_train = to_categorical(y_train, 5)
+    y_val = to_categorical(y_val, 5)
+    y_test = to_categorical(y_test, 5)
+
+    x_train = x_train.astype("float32") / 255
+    x_val = x_val.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
